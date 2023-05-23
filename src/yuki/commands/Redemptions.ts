e22ab20f9345597522f526fdfb9f823c308ecd30
JonoAugustine <me@jonoaugustine.com>
2023-05-22 23:27:51 -0400

    fix: remove spent money for pushups command
    fixes pushup alias

--- src/yuki/commands/Redemptions.ts
+++ src/yuki/commands/Redemptions.ts
@@ -2,6 +2,7 @@ import { YukiBuilder } from "@pinkilo/yukibot"
 import supercommand from "./supercommand"
 import { enqueueNewAlert } from "../alerts"
 import MoneySystem from "../MoneySystem"
+import MS from "../MoneySystem"
 
 export const FitCheck = (y: YukiBuilder) =>
   y.command((c) => {
@@ -25,19 +26,22 @@ export const Hydrate = (y: YukiBuilder) =>
     c.name = "hydrate"
     c.alias = ["drink", "water", "drinkwater"]
     c.rateLimit.global = 60 * 5
-    supercommand(c, 10, async ({ authorDetails }) => {
-      return await enqueueNewAlert(
-        "Hydrate!",
-        authorDetails.displayName,
-        authorDetails.channelId
-      )
-    })
+    supercommand(
+      c,
+      10,
+      async ({ authorDetails }) =>
+        await enqueueNewAlert(
+          "Hydrate!",
+          authorDetails.displayName,
+          authorDetails.channelId
+        )
+    )
   })
 
 export const Pushups = (y: YukiBuilder) =>
   y.command((c) => {
     c.name = "pushups"
-    c.alias = ["pushups"]
+    c.alias = ["pushup"]
     c.rateLimit.global = 60 * 10
     c.rateLimit.individual = 60 * 60
     const defaultCount = 10
@@ -50,6 +54,7 @@ export const Pushups = (y: YukiBuilder) =>
       },
       async ({ authorDetails: { channelId, displayName } }, tokens, cost) => {
         const count = parseInt(tokens.params[0]) || defaultCount
+        await MS.transactionBatch([[channelId, cost]])
         await enqueueNewAlert(`Pushups: ${count}`, displayName, channelId)
         await y.sendMessage(
           `${displayName} redeemed ${count} pushups for ${cost} ${MoneySystem.name}s`
