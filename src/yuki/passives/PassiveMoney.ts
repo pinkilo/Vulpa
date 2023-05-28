576c3b5e20036bbd445510653b5dfd1bd123cc3e
JonoAugustine <me@jonoaugustine.com>
2023-05-28 01:24:16 -0400

    fix: passive money cooldown ignores bot messages

--- src/yuki/passives/PassiveMoney.ts
+++ src/yuki/passives/PassiveMoney.ts
@@ -1,6 +1,7 @@
 import { YukiBuilder } from "@pinkilo/yukibot"
 import logger from "../../logger"
 import MoneySystem from "../MoneySystem"
+import Env from "../../env"
 
 const moneyCooldown = 2
 /** amount of money earned per cooldown */
@@ -9,16 +10,17 @@ const moneyEarnRate = 2
 export default (y: YukiBuilder) =>
   y.memoryPassive<Map<string, number>>(
     new Map(),
-    async (_, { isCommand }) => !isCommand,
+    async ({ authorDetails: { channelId } }, { isCommand }) =>
+      !isCommand && channelId !== Env.SELF.ID,
     async ({ authorDetails: { channelId } }, _, { memory: cooldowns }) => {
       for (let uid of cooldowns.keys()) {
         if (uid !== channelId) cooldowns.set(uid, cooldowns.get(uid) - 1)
         if (cooldowns.get(uid) < 1) cooldowns.delete(uid)
       }
-      if (!cooldowns.has(channelId)) {
-        logger.debug(`adding passive money to ${channelId}`)
-        await MoneySystem.transact().deposit(channelId, moneyEarnRate).execute()
-        cooldowns.set(channelId, moneyCooldown)
-      }
+      if (cooldowns.has(channelId)) return
+
+      logger.debug(`adding passive money to ${channelId}`)
+      await MoneySystem.transact().deposit(channelId, moneyEarnRate).execute()
+      cooldowns.set(channelId, moneyCooldown)
     }
   )
