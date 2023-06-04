9d571c9debb5d049200117c318b33a2fbebf9fd1
JonoAugustine <me@jonoaugustine.com>
2023-06-04 04:54:20 -0400

    test(quest): add more quest system tests

--- src/yuki/rpg/quests/Quest.ts
+++ src/yuki/rpg/quests/Quest.ts
@@ -6,13 +6,15 @@ type Quest = {
   /** entry cost */
   readonly cost: number
   joinMessage(name: string): string
-  step(): Promise<QuestStatus>
+  announceMessage(): string
+  step(curStatus: QuestStatus): Promise<QuestStatus>
 }
 
 export const Heist: Quest = {
   playerIDs: new Set(),
   limits: [3],
   cost: 100,
+  announceMessage: () => "A heist is starting! Join for the chance of a payout!",
   joinMessage: (name: string): string => `${name} joins the heist!`,
   async step() {
     return QuestStatus.ANNOUNCED
